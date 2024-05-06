144cee7399ba75fc495afeca60d5b80d941733e2
Saikiran Palvai <137756791+skreddypalvai@users.noreply.github.com>
2024-05-06 05:59:32 -0400

    Removed project details

--- Project_Tasks/task_4_quizbuilding.py
+++ Project_Tasks/task_4_quizbuilding.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     # Configuration for EmbeddingClient
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "my-quiz-project-420005",
-        "location": "us-central1"
+        "project": "Enter your project ID here",
+        "location": "your location"
     }
     
     screen = st.empty() # Screen 1, ingest documents
